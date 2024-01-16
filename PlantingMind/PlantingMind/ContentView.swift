b2e9e73eefe116838fdead71c27cd1bdebc1a37f
eunjoo-choi <epcej0020@gmail.com>
2024-01-16 22:14:51 +0900

    프로젝트 생성

---
+++ PlantingMind/PlantingMind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantingMind
+//
+//  Created by 최은주 on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantingMind/PlantingMind/PlantingMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantingMindApp.swift
+//  PlantingMind
+//
+//  Created by 최은주 on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantingMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
