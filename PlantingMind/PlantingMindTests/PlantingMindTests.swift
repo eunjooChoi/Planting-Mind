46602f053f731c5e187b4baadaac74f08ccd7eeb
eunjoo-choi <epcej0020@gmail.com>
2024-01-17 13:04:31 +0900

    github action 테스트를 위한 테스트코드 수정

--- PlantingMind/PlantingMindTests/PlantingMindTests.swift
+++ PlantingMind/PlantingMindTests/PlantingMindTests.swift
@@ -28,9 +28,7 @@ final class PlantingMindTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssert(true)
     }
 
 }
