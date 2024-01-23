0d03521938e4c28d3a44cbd9f52cb6d7d6e1f965
eunjoo-choi <epcej0020@gmail.com>
2024-01-23 15:03:12 +0900

    Custom Color 추가
    ## 수정 사항
    - Assets에 기분을 나타내는 Color 추가
    - Color("ColorName")으로 직접 사용하지 않도록 ColorExtension 추가

---
+++ PlantingMind/PlantingMind/ColorExtension.swift
@@ -0,0 +1,38 @@
+//
+//  ColorExtension.swift
+//  PlantingMind
+//
+//  Created by 최은주 on 1/23/24.
+//
+
+import Foundation
+import SwiftUI
+
+private enum CustomColor: String {
+    case background
+    case line
+    case general
+    case point
+    case bad
+    case notBad
+    case normal
+    case good
+    case nice
+}
+
+extension Color {
+    struct Custom {
+        // Basic Color
+        static let background = Color(CustomColor.background.rawValue)
+        static let line = Color(CustomColor.line.rawValue)
+        static let general = Color(CustomColor.general.rawValue)
+        static let point = Color(CustomColor.point.rawValue)
+        
+        // Mood Color
+        static let bad = Color(CustomColor.bad.rawValue)
+        static let notBad = Color(CustomColor.notBad.rawValue)
+        static let normal = Color(CustomColor.normal.rawValue)
+        static let good = Color(CustomColor.good.rawValue)
+        static let nice = Color(CustomColor.nice.rawValue)
+    }
+}
