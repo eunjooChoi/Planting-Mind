9db4bb585ed72e8e2d4877f0dea86fceef2a8948
eunjoo-choi <epcej0020@gmail.com>
2024-01-26 20:29:57 +0900

    날짜를 누르면 감정 기록 화면이 뜨도록 MoodRecordView 생성

--- PlantingMind/PlantingMind/Calender/DayCellView.swift
+++ PlantingMind/PlantingMind/Calender/DayCellView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct DayCellView: View {
+    @State var showMoodRecordView: Bool = false
     var calendarModel: CalendarModel
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 10)
-            .fill(.clear)
-            .overlay {
-                Text("\(calendarModel.day)")
-                    .frame(width: 30, height: 30)
-                    .fontWeight(.semibold)
-                    .background(calendarModel.isToday ? Color.Custom.general : .clear)
-                    .foregroundStyle(calendarModel.isToday ? Color.Custom.point : Color.Custom.general)
-                    .clipShape(Circle())
-            }
-            .frame(height: 50)
+        Button(action: {
+            showMoodRecordView.toggle()
+        }, label: {
+            Text("\(calendarModel.day)")
+                .frame(width: 30, height: 30)
+                .fontWeight(.semibold)
+                .background(calendarModel.isToday ? Color.Custom.general : .clear)
+                .foregroundStyle(calendarModel.isToday ? Color.Custom.point : Color.Custom.general)
+                .clipShape(Circle())
+        })
+        .frame(height: 50)
+        .sheet(isPresented: $showMoodRecordView) {
+            MoodRecordView()
+        }
     }
 }
 
