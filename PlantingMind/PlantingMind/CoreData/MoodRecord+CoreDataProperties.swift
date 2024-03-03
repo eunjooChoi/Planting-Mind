a716719368f109546c2e9329cc1ae0f0a45e4cfd
eunjoo-choi <epcej0020@gmail.com>
2024-03-03 22:20:44 +0900

    MoodRecordViewModelTest 파일 추가
    ## 수정 사항
    - MoodRecordViewModel 파일 생성
       - ViewModel에서 context를 가질 수 있도록 DayCellView에서 MoodRecordView로 넘어올 때 View 모델에 context를 주입
       - 코어데이터에 저장 시 Date 초기화를 위해 calendarModel도 함께 받도록 추가
    
    - xcdatamodel 수정
       - mood를 String 타입으로 변경
       - CoreDataProperties와 CoreDataClass 추가
           - Date와 mood를 non-optional로 관리하기 위해 manual로 생성

---
+++ PlantingMind/PlantingMind/CoreData/MoodRecord+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  MoodRecord+CoreDataProperties.swift
+//  PlantingMind
+//
+//  Created by 최은주 on 3/3/24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension MoodRecord {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MoodRecord> {
+        return NSFetchRequest<MoodRecord>(entityName: "MoodRecord")
+    }
+
+    @NSManaged public var date: Date
+    @NSManaged public var mood: String
+    @NSManaged public var reason: String?
+
+}
+
+extension MoodRecord : Identifiable {
+
+}
---
+++ PlantingMind/PlantingMind/MoodRecord/Mood.swift
@@ -0,0 +1,16 @@
+//
+//  Mood.swift
+//  PlantingMind
+//
+//  Created by 최은주 on 3/3/24.
+//
+
+import Foundation
+
+enum Mood: String, CaseIterable {
+    case nice
+    case good
+    case normal
+    case notBad
+    case bad
+}
