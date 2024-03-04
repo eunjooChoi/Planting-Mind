cc73e75346de6aff75168bcdda5ec3392aebe4d3
eunjoo-choi <epcej0020@gmail.com>
2024-03-04 15:04:16 +0900

    test 실패했을 때 원인을 알 수 있도록 xcresulttool 코드를 github action worksflow 파일에 추가
    - 실패하는 샘플 테스트 코드는 테스트 이후 삭제 예정

---
+++ PlantingMind/PlantingMindTests/SampleTest.swift
@@ -0,0 +1,25 @@
+//
+//  SampleTest.swift
+//  PlantingMindTests
+//
+//  Created by 최은주 on 3/4/24.
+//
+
+import XCTest
+
+final class SampleTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let wrongExpectation = false
+        XCTAssertTrue(wrongExpectation)
+    }
+
+}
