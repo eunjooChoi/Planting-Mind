3ebe164427bc9e34a7700c6d447ad4b7acbee762
eunjoo-choi <epcej0020@gmail.com>
2024-03-09 20:00:48 +0900

    MoodRecordViewModel 리팩토링
    - Calendar 화면에서 MoodRecord를 받아서 사용하도록 수정
    - 더이상 사용하지 않는 Fetch 메서드 제거
    - 테스트코드 수정

--- PlantingMind/PlantingMind/MoodRecord/MoodRecordViewModel.swift
+++ PlantingMind/PlantingMind/MoodRecord/MoodRecordViewModel.swift
@@ -10,58 +10,29 @@ import CoreData
 
 class MoodRecordViewModel: ObservableObject {
     private let context: NSManagedObjectContext
-    private let calendarModel: CalendarModel
     private let date: Date?
-    private var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        
-        return formatter
-    }
     
     @Published var mood: Mood
     @Published var reason: String
     
-    init(context: NSManagedObjectContext, calendarModel: CalendarModel) {
+    init(context: NSManagedObjectContext, calendarModel: CalendarModel, moodRecord: MoodRecord?) {
         self.context = context
-        self.calendarModel = calendarModel
         self.date = Calendar.current.date(from: DateComponents(year: calendarModel.year,
                                                                month: calendarModel.month,
                                                                day: calendarModel.day))
-        self.mood = .normal
-        self.reason = ""
-    }
-    
-    func fetch() {
-        guard let date = date else { return }
         
-        let timestamp = dateFormatter.string(from: date)
-        
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "MoodRecord")
-        let predicate = NSPredicate(format: "timestamp == %@", timestamp)
-        fetchRequest.predicate = predicate
-        
-        do {
-            guard let result = try context.fetch(fetchRequest) as? [MoodRecord],
-                  let record = result.first else { return }
-            
-            self.mood = Mood(rawValue: record.mood) ?? .normal
-            self.reason = record.reason ?? ""
-            
-        } catch {
-            print("Failed to fetch the mood record", error.localizedDescription)
-        }
+        self.mood = Mood(rawValue: moodRecord?.mood ?? Mood.normal.rawValue) ?? .normal
+        self.reason = moodRecord?.reason ?? ""
     }
     
     func save() {
-        guard let entity = NSEntityDescription.entity(forEntityName: "MoodRecord", in: context) else { return }
-        guard let date = date else { return }
-        let timestamp = dateFormatter.string(from: date)
+        guard let date = self.date else { return }
+        let timestamp = date.timeStampString()
         
-        let object = NSManagedObject(entity: entity, insertInto: context)
-        object.setValue(timestamp, forKey: "timestamp")
-        object.setValue(mood.rawValue, forKey: "mood")
-        object.setValue(reason, forKey: "reason")
+        let record = MoodRecord(context: context)
+        record.timestamp = timestamp
+        record.mood = self.mood.rawValue
+        record.reason = self.reason
         
         do {
             try context.save()
