29cdb1b324866f28481832d2e22ccc898c1320a5
eunjoo-choi <epcej0020@gmail.com>
2024-03-11 19:52:30 +0900

    오늘의 날짜보다 미래의 날짜인 경우에는 기분 기록을 할 수 없도록 disabled 처리

--- PlantingMind/PlantingMindTests/DayCellViewModelTest.swift
+++ PlantingMind/PlantingMindTests/DayCellViewModelTest.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 final class DayCellViewModelTest: XCTestCase {
     func test_DayCellModel_record_nil() throws {
-        let model = DayCellViewModel(calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
+        let model = DayCellViewModel(today: Date(), calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
                                      moodRecord: nil)
         
         XCTAssertNil(model.mood)
@@ -25,7 +25,7 @@ final class DayCellViewModelTest: XCTestCase {
         moodRecord.timestamp = "2024-02-24"
         moodRecord.mood = Mood.nice.rawValue
         
-        let model = DayCellViewModel(calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
+        let model = DayCellViewModel(today: Date(), calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
                                      moodRecord: moodRecord)
         
         XCTAssertEqual(Mood.nice, model.mood)
@@ -34,7 +34,7 @@ final class DayCellViewModelTest: XCTestCase {
     }
     
     func test_isToday_true_날짜색() throws {
-        let model = DayCellViewModel(calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: true),
+        let model = DayCellViewModel(today: Date(), calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: true),
                                      moodRecord: nil)
         
         XCTAssertEqual(model.dayForegroundColor, Color.Custom.point)
@@ -42,10 +42,46 @@ final class DayCellViewModelTest: XCTestCase {
     }
     
     func test_isToday_false_날짜색() throws {
-        let model = DayCellViewModel(calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
+        let model = DayCellViewModel(today: Date(), calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
                                      moodRecord: nil)
         
         XCTAssertEqual(model.dayForegroundColor, Color.Custom.general)
         XCTAssertEqual(model.dayBackgroundColor, .clear)
+    }
+    
+    func test_미래_날짜색은_회색() throws {
+        let today = try XCTUnwrap(Calendar.current.date(from: DateComponents(year: 2024, month: 1, day: 24)))
+        let model = DayCellViewModel(today: today, calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
+                                     moodRecord: nil)
+        
+        XCTAssertEqual(model.dayForegroundColor, Color.gray)
+    }
+    
+    func test_isFutureDate_보여줄_날짜가_오늘보다_과거() throws {
+        let model = DayCellViewModel(today: Date(),
+                                     calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
+                                     moodRecord: nil)
+        
+        XCTAssertFalse(model.isFutureDate)
+    }
+    
+    func test_isFutureDate_보여줄_날짜가_오늘보다_미래() throws {
+        let today = try XCTUnwrap(Calendar.current.date(from: DateComponents(year: 2024, month: 1, day: 1)))
+        
+        let model = DayCellViewModel(today: today,
+                                     calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: false),
+                                     moodRecord: nil)
+        
+        XCTAssertTrue(model.isFutureDate)
+    }
+    
+    func test_isFutureDate_보여줄_날짜가_오늘() throws {
+        let today = try XCTUnwrap(Calendar.current.date(from: DateComponents(year: 2024, month: 2, day: 24)))
+        
+        let model = DayCellViewModel(today: today,
+                                     calendarModel: CalendarModel(year: 2024, month: 2, day: 24, isToday: true),
+                                     moodRecord: nil)
+        
+        XCTAssertFalse(model.isFutureDate)
     }
 }
