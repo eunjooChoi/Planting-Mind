dd175f62539ca5972b71de25482b14918d6a8b22
eunjoo-choi <epcej0020@gmail.com>
2024-03-20 18:06:28 +0900

    systemsmall 사이즈의 위젯 가디자인

--- PlantingMind/PlantingWidget/PlantingWidget.swift
+++ PlantingMind/PlantingWidget/PlantingWidget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), emoji: "😀")
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), emoji: "😀")
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -28,7 +28,7 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate, emoji: "😀")
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -41,21 +41,31 @@ struct SimpleEntry: TimelineEntry {
 
 struct PlantingWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+        HStack {
+            VStack(spacing: 4) {
+                ForEach(Calendar.current.veryShortWeekdaySymbols, id: \.self) { symbol in
+                    Text(symbol)
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                }
+            }
+            
+            LazyHGrid(rows: Array(repeating: GridItem(.flexible(maximum: 15), spacing: 3), count: 7), spacing: 4, content: {
+                ForEach(0...31, id: \.self) { item in
+                    RoundedRectangle(cornerRadius: 2)
+                        .foregroundColor(Color.cyan)
+                        .frame(width: 18)
+                }
+            })
         }
     }
 }
 
 struct PlantingWidget: Widget {
     let kind: String = "com.eunjoo.planting-mind.PlantingWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
