36aa79bf5219bee5c9abe8f63c7e782e76fa38fa
eunjoo-choi <epcej0020@gmail.com>
2024-03-20 17:33:38 +0900

    위젯 초기 세팅
    - 위젯 익스텐션 추가
    - PlantingMind 타겟과 CordData 공유를 위해 AppGroup 추가

---
+++ PlantingMind/PlantingWidget/PlantingWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PlantingWidgetBundle.swift
+//  PlantingWidget
+//
+//  Created by 최은주 on 3/19/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PlantingWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PlantingWidget()
+    }
+}
